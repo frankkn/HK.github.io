13d40f7336946c8aa79c5569f829c2653d4ab892
Frank Yang <37926170+frankkn@users.noreply.github.com>
2021-08-23 17:39:14 +0800

    Add files via upload

--- HK/date.js
+++ HK/date.js
@@ -1,61 +1,38 @@
-//抓年、月、日
-var date = new Date();
-var month = date.getMonth();
-var year = date.getFullYear();
-var day = date.getDate();
-//改成文字
-var dayName= date.toLocaleString(navigator.language,{weekday:'long'});
-var monthName= date.toLocaleString(navigator.language,{month:'long'});
+function updateTime(){
+    var time = new Date();
+    var dname = time.getDay(),
+        mo = time.getMonth(),
+        yr = time.getFullYear(),
+        dnum = time.getDate(),
+        hr = time.getHours(),
+        min = time.getMinutes(),
+        sec = time.getSeconds(),
+        pe = "AM";
 
-//英文轉換成中文
+    // 抓AM或PM
+    if(hr >= 12){
+        pe = "PM";
+    }else{
+        pe = "AM";
+    }
 
-var C_dayName;  
-var C_monthName;
-/*
-if (dayName == "Monday") {
-    C_dayName = "星期一";
-} else if (dayName == "Tuesday") {
-    C_dayName = "星期二";
-} else if (dayName == "Wednesday") {
-    C_dayName = "星期三";
-} else if (dayName == "Thursday") {
-    C_dayName = "星期四";
-} else if (dayName == "Friday") {
-    C_dayName = "星期五";
-} else if (dayName == "Saturday") {
-    C_dayName = "星期六";
-} else {
-    C_dayName = "星期日";
-};
-*/
-if (monthName == "1月") {
-    C_monthName = "一月";
-} else if (monthName == "2月") {
-    C_monthName = "二月";
-} else if (monthName == "3月") {
-    C_monthName = "三月";
-} else if (monthName == "4月") {
-    C_monthName = "四月";
-} else if (monthName == "5月") {
-    C_monthName = "五月";
-} else if (monthName == "6月") {
-    C_monthName = "六月";
-} else if (monthName == "7月") {
-    C_monthName = "七月";
-} else if (monthName == "8月") {
-    C_monthName = "八月";
-} else if (monthName == "9月") {
-    C_monthName = "九月";
-} else if (monthName == "10月") {
-    C_monthName = "十月";
-} else if (monthName == "11月") {
-    C_monthName = "十一月";
-} else {
-    C_monthName = "十二月";
+    // 轉換成兩位數
+    Number.prototype.pad = function(digits){
+        for(var n = this.toString(); n.length<digits; n = 0+n);
+        return n;
+    }
+
+    var months = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+    var week = ["星期日","星期一","星期二","星期三","星期四","星期五","星期六"];
+    var ids = ["dayname","month","daynum","year","hour","minutes","seconds","period"];
+    var values = [week[dname], months[mo], dnum.pad(2), yr, hr.pad(2), min.pad(2), sec.pad(2), pe];
+
+    for (var i = 0; i < ids.length; i++){
+        document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+    }
 }
 
-//呈現內容
-document.getElementById('day').innerHTML=day;
-document.getElementById('year').innerHTML=year;
-document.getElementById('monthName').innerHTML=C_monthName;
-document.getElementById('dayName').innerHTML=dayName;
+function startTime(){
+    updateTime();
+    window.setInterval("updateTime()",1);
+}
